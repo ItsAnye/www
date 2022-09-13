0881ec4419692a435b4d851fe32562b892b9b9a8
Anay Mittal <anayttal@gmail.com>
2022-09-13 12:53:08 -0700

    Tons of progress, adding play button on game page

---
+++ games/main.js
@@ -0,0 +1,17 @@
+const urlParams = new URLSearchParams(window.location.search);
+let gamename = urlParams.get('name');
+let desc = urlParams.get('desc');
+let creators = urlParams.get('creators').slice(1, -1).replace(/['"]+/g, '').split(',');
+
+//Image + Title
+document.body.innerHTML += `<img src="../images/thumbnails/${gamename.replace(' ', '_')}.png" style="border: 5px solid black; margin-left: calc(80px + 100px); margin-top: 100px; width: 300px;">`
+document.body.innerHTML += `<h1 class="unselectable" style="position: absolute; margin-left: 500px; margin-top: -300px;">${gamename}</h1>`
+
+//Creators
+if(creators.length > 1){
+    document.body.innerHTML += `<h3 class="unselectable" style="position: absolute; margin-left: 505px; margin-top: -270px;">By: ${creators[0]} <p>+${creators.length - 1}</p></h3>`
+} else {
+    document.body.innerHTML += `<h3 class="unselectable" style="position: absolute; margin-left: 505px; margin-top: -250px;">By: ${creators[0]}</h3>`
+}
+
+//
