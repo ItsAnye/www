bc3d373c8c099097471e207bdb4f7f50687666f4
Anay Mittal <anayttal@gmail.com>
2022-09-28 17:00:03 -0700

    Finished lesson 1, working on learn home page

--- learn/html_css/main.js
+++ learn/html_css/main.js
@@ -1,9 +1,12 @@
+let snippets = document.getElementsByTagName("meltdown-code");
 let arr = ['one', 'two', 'three', 'four', 'five'];
+let lesson = 'one';
+
+let answers = {};
+let tasks = document.getElementById('tasks');
 
 let one = document.getElementById("one");
-let snippets = document.getElementsByTagName("meltdown-code");
 one.innerHTML += `<h1>This is an h1 tag!</h1>`;
-let lesson = 'one'
 
 function formatSnippets(){
     for(let i = 0; i < snippets.length; i++) {
@@ -47,18 +50,47 @@ function formatSnippets(){
     }
 }
 
+function createTask(task, final="undef"){
+    tasks.innerHTML += `<div id="task_${tasks.children.length + 1}"><div id="todo_icon"><i class="fa-solid fa-check"></i></div>&nbsp<span>${task}</span><br></div>`;
+    answers[`task_${tasks.children.length}`] = final;
+}
+
+function createInput(type, pre, placeholder, ans1, ans2, final1, final2){
+    tasks.innerHTML += `<div id="task_${tasks.children.length + 1}"><div id="todo_icon"><i class="fa-solid fa-check"></i></div>&nbsp<span>${pre}</span><div><input id="task_${tasks.children.length + 1}_input_${type}" class="task_input" type="${type}" placeholder="${placeholder}"><button class="task_button" onclick="inputSubmitted('${ans1}', '${ans2}', '${type}', '${tasks.children.length + 1}', '${final1}', '${final2}')">Submit</button></div><br></div>`;
+}
+
+function inputSubmitted(ans1, ans2, type, index, final1, final2){
+    let val = document.getElementById(`task_${index}_input_${type}`).value;
+    document.getElementById(`task_${index}_input_${type}`).value = '';
+    document.getElementById(`task_${index}_input_${type}`).parentElement.parentElement.innerHTML = `<div id="todo_icon"><i class="fa-solid fa-check"></i></div>&nbsp${final1}<code>&lt;${ans1}&gt;${val}&lt;${ans2}&gt;</code>${final2}`
+    answers[`task_${tasks.children.length}`] = decodeHtml(`&lt;${ans1}&gt;${val}&lt;${ans2}&gt;`);
+
+}
+
 formatSnippets();
+
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
 
 //Live Reload
 function refresh() {
     let editor_value = document.getElementById('editor-textarea').value;
     document.getElementById('viewer').srcdoc = editor_value;
 
-    if(lesson == 'one' && editor_value == '<h1>Hello World!</h1>'){
-        document.getElementById('item_one').disabled = false;
-        document.getElementById('item_one').style.pointerEvents = 'none';
-    } else {
-        document.getElementById('item_one').disabled = true;
-        document.getElementById('item_one').style.pointerEvents = 'none';
+    console.log(answers)
+
+    for (const [key, value] of Object.entries(answers)) {
+        if(editor_value.includes(value)){
+            document.getElementById(key).firstChild.style.backgroundColor = "rgb(0, 196, 0)";
+        } else {
+            document.getElementById(key).firstChild.style.backgroundColor = "lightgray";
+        }
     }
 }
+
+//Tasks
+createTask('Type <code>Hello World!</code> between the opening and closing tags.', '<h1>Hello World!</h1>');
+createInput('input', 'Please Enter Your Name: ', 'Name here...', 'h1', '/h1', 'Type ', ' into your project.')
